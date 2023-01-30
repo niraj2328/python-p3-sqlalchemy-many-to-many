e42533520ed5d7e7e225f13a1d9da2ea0e578aae
Niraj <niraj.patel@flatironschool.com>
2023-01-30 09:49:12 -0500

    Completed

--- lib/models.py
+++ lib/models.py
@@ -1,11 +1,20 @@
-from sqlalchemy import create_engine, func
-from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String, DateTime, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 engine = create_engine('sqlite:///many_to_many.db')
 
 Base = declarative_base()
+
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Game(Base):
     __tablename__ = 'games'
@@ -16,6 +25,7 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
+    users = relationship('User', secondary=game_user, back_populates='games')
     reviews = relationship('Review', backref=backref('game'))
 
     def __repr__(self):
@@ -31,8 +41,26 @@ class Review(Base):
     comment = Column(String())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
+    user_id = Column(Integer(), ForeignKey('users.id'))
 
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'))
+
+    # don't forget your __repr__()!
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
+
